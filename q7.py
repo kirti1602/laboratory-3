6fb3b1b856ca0e7ca5035bb460248c4dc197ad1e
kirti1602 <kirtikashyap9a@gmail.com>
2025-02-24 12:28:18 +0530

    Create q7.py

---
+++ q7.py
@@ -0,0 +1,7 @@
+# Read radius of a circle and print the area
+import math
+
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius**2  # Area of a circle formula
+
+print(f"The area of the circle with radius {radius} is {area}")
