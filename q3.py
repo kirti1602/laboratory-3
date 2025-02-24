56b428afe3ed5449c2a2c51d6d271610904167c4
kirti1602 <kirtikashyap9a@gmail.com>
2025-02-24 12:26:24 +0530

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,6 @@
+# Read two numbers and find the sum of their cubes
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum_of_cubes = num1**3 + num2**3
+print(f"The sum of the cubes of {num1} and {num2} is {sum_of_cubes}")
